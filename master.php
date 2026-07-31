5e28a0c886e5efe3eaf6401a366040ff2441289f
KajalLad1206 <kajallad1994@gmail.com>
2026-07-30 22:57:11 -0400

    revert example -add in master.php file

--- master.php
+++ master.php
@@ -4,6 +4,7 @@ i am changing from login branch
 
 this is example of revert and reset
 
+e0a60af (HEAD -> feature/student, origin/feature/student) revert example
 
 now, revert example,
 my HEAD has to be now 754da71 after push and revert command
